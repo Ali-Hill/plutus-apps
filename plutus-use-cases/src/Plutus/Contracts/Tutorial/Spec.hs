49ed5ac87026475a40339d876b92c3e735c434f6
Alasdair Hill <alasdair.hill@iohk.io>
2023-02-20 16:42:34 +0000

    Update Spec.hs
    Removing tests from certification for faster testing

--- plutus-use-cases/src/Plutus/Contracts/Tutorial/Spec.hs
+++ plutus-use-cases/src/Plutus/Contracts/Tutorial/Spec.hs
@@ -391,13 +391,8 @@ prop_UnitTest = withMaxSuccess 1 $ forAllDL unitTest2 prop_Escrow
 -- | Certification.
 certification :: Certification EscrowModel
 certification = defaultCertification {
-    certNoLockedFunds = Just noLockProof,
-    certCrashTolerance = Just Instance,
-    certUnitTests = Just unitTest,
-    certDLTests = [("redeem test", unitTest1), ("refund test", unitTest2)],
     certCoverageIndex      = covIdx
   }
-  where unitTest _ = tests
 
 check_propEscrowWithCoverage :: IO ()
 check_propEscrowWithCoverage = do
